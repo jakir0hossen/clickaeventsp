2fafcf92cfaee8a8bb99f78532a48edb1e773a82
Jakir-Hossen-Developer <jhbjakir@gmail.com>
2024-01-24 04:36:15 +0600

    event app run

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:clickaeventsp/firebase_options.dart';
+import 'package:clickaeventsp/screen/main%20manu/calander.dart';
+import 'package:clickaeventsp/screen/main%20manu/mainScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/emailVerificationScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/loginScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/pinVerificationScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/registrationScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/setPasswordScreen.dart';
+import 'package:clickaeventsp/screen/onboarding/splashScreen.dart';
+import 'package:clickaeventsp/screen/profile/ProfilePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'navbar.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blueAccent),
+       title: "Click A Event",
+       initialRoute :'/Calendar',
+       routes:{
+
+         '/splashScreen':(context)=> splashScreen(),
+         '/login':(context)=> loginScreen(),
+         '/registration':(context)=> registrationScreen(),
+         '/emailVerification':(context)=> emailVerificationScreen(),
+         '/setPassword':(context)=> setPasswordScreen(),
+         '/pinVerification':(context)=> pinVerificationScreen(),
+         '/profilePage':(context)=> ProfilePage(),
+         '/mainScreen':(context)=> mainScreen(),
+         '/Navbar':(context)=> Navbar(),
+         '/Calendar':(context)=> Calendar(),
+
+       }
+      
+    );
+  }
+}
+
---
+++ lib/screen/main manu/mainScreen.dart
@@ -0,0 +1,83 @@
+
+
+import 'package:clickaeventsp/screen/main%20manu/searchBar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// import '../profile/ProfilePage.dart';
+import '../../navbar.dart';
+import 'HomePage.dart';
+// import 'calander.dart';
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  MySnakesBar(massage,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(massage))
+    );
+  }
+
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  var currentIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+     return Scaffold(
+       drawer: Navbar(),
+       appBar: AppBar(
+         backgroundColor: Colors.blueAccent,
+         title: Text("Click A Event",style: TextStyle(color: Colors.white),),
+
+         actions: [
+           IconButton(onPressed: (){
+             showSearch(
+                 context: context,
+                 delegate: Search());
+           },
+               icon: Icon(Icons.search))
+         ],
+
+
+       ),
+       backgroundColor: Colors.white,
+       bottomNavigationBar: BottomNavigationBar(
+
+         currentIndex: currentIndex,
+
+         type: BottomNavigationBarType.fixed,
+
+         onTap: (index){
+           currentIndex = index;
+         },
+
+         items: [
+
+           BottomNavigationBarItem(icon: Icon(Icons.home),label:"Home" ),
+           BottomNavigationBarItem(icon: Icon(Icons.search),label:"Search" ),
+           BottomNavigationBarItem(icon: Icon(Icons.calendar_view_month_rounded),label:"Calendar" ),
+           BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined),label:"Account", ),
+           BottomNavigationBarItem(icon: Icon(Icons.menu),label:"Menu" ),
+
+
+         ],
+
+       ),
+
+
+       body: getBodyWiget(),
+     );
+  }
+  getBodyWiget() {
+  return (currentIndex==0)? HomePage():Container();
+  }
+
+}
